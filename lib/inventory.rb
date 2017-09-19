f55570e1af61857143bc1330e6640832ffcbdd0a
Marshall Houston <marshall.houston@gmail.com>
2017-09-19 09:41:25 -0600

    changed record_item method based on failure with adding two items

--- lib/inventory.rb
+++ lib/inventory.rb
@@ -9,7 +9,11 @@ class Inventory
   end
 
   def record_item(item_info)
-    @items.merge!(item_info)
+    # @items.merge!(item_info){|key, oldval, newval| oldval.merge!(newval){|key, oldval, newval| oldval + newval}}
+    all_item_info = []
+    all_item_info << item_info
+    @items = all_item_info.first if all_item_info.count == 1
   end
+
 
 end
